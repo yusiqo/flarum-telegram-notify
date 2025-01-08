0a6f07046fe9e140b31d953757cbfa48bc1a82e5
yusiqo <80514373+yusiqo@users.noreply.github.com>
2025-01-08 23:39:30 +0300

    Create AddSettingsPage.php

---
+++ src/Admin/AddSettingsPage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TelegramNotify\Admin;
+
+use Flarum\Extend\Frontend;
+use Flarum\Frontend\Document;
+
+class AddSettingsPage
+{
+    public function __invoke(Document $document)
+    {
+        $document->payload['settings'] = [
+            'bot_token' => app('flarum.settings')->get('telegram-notify.bot_token'),
+            'chat_id' => app('flarum.settings')->get('telegram-notify.chat_id'),
+        ];
+    }
+}
