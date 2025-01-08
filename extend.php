578cdc11fdc7792e98808e456f517c08151b7876
yusiqo <80514373+yusiqo@users.noreply.github.com>
2025-01-09 00:24:18 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -10,6 +10,18 @@ return [
         ->serializeToForum('telegramNotifyMessageTemplate', 'telegram-notify.message_template')
         ->serializeToForum('telegramNotifyButtonText', 'telegram-notify.button_text'),
 
+    (new Extend\Frontend('admin'))
+        ->content(function ($document) {
+            $settings = resolve('flarum.settings');
+            $document->payload['telegramNotifySettings'] = [
+                'bot_token' => $settings->get('telegram-notify.bot_token'),
+                'chat_id' => $settings->get('telegram-notify.chat_id'),
+                'message_template' => $settings->get('telegram-notify.message_template', "Yeni bir gönderi oluşturuldu:\n\nBaşlık: {title}\nYazar: {author}\nMesaj: {content}\nBağlantı: {link}"),
+                'button_text' => $settings->get('telegram-notify.button_text', 'Gönderiyi Görüntüle'),
+            ];
+        }),
+
+
     (new Extend\Event())
         ->listen(\Flarum\Post\Event\Posted::class, \TelegramNotify\Listeners\PostCreatedListener::class),
 ];
